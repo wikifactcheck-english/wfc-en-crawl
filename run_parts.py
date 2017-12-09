56f5b8a61de8c1a30c5a3739d7517e494aeddbff
Nathan Perry <avaglir@gmail.com>
2017-12-09 15:46:07 -0500

    actually fix env

--- run_parts.py
+++ run_parts.py
@@ -9,10 +9,13 @@ ct = 0
 
 procs = []
 
+env = os.environ
+env['GOMAXPROCS'] = '4'
+
 for filename in os.listdir('indices'):
     logf = open('logs/{}.log'.format(ct), 'w')
     proc = subprocess.Popen(shlex.split('go run wikite.go -badFile bad/{}.txt -indexFile indices/{}'.format(ct, filename)),
-                            stdout=logf, stderr=logf, env={'GOMAXPROCS': '4'})
+                            stdout=logf, stderr=logf, env=env)
 
     procs.append(proc)
 
